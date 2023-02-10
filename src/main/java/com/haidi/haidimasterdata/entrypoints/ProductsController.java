02539bfd0825324e715b44d10a7edfb5071a3a27
Sid Be <sa.bennaceur@gmail.com>
2023-02-10 13:18:39 +0100

    code

--- src/main/java/com/haidi/haidimasterdata/entrypoints/ProductsController.java
+++ src/main/java/com/haidi/haidimasterdata/entrypoints/ProductsController.java
@@ -26,6 +26,7 @@ public class ProductsController {
     @ApiResponse(responseCode = "200", description = "Created product database Identifier")
     @PostMapping(value = "/createProduct", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public Long createProduct(@RequestBody CreateProductRequest productRequest){
+
         final var product = Product.createOne(
                 productRequest.code,
                 productRequest.description,
