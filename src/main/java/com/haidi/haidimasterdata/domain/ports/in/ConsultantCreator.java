0ae97de7c0e3eb4f82970dbd5665340c0c4d6969
Sid Be <sa.bennaceur@gmail.com>
2023-03-08 16:32:34 +0100

    remapping with new db

--- src/main/java/com/haidi/haidimasterdata/domain/ports/in/ConsultantCreator.java
+++
@@ -1,7 +0,0 @@
-package com.haidi.haidimasterdata.domain.ports.in;
-
-import com.haidi.haidimasterdata.domain.pojo.CreateConsultantCommand;
-
-public interface ConsultantCreator {
-    Long saveConsultant(CreateConsultantCommand createConsultantCommand);
-}
--- src/main/java/com/haidi/haidimasterdata/domain/ports/out/ConsultantPersistencePort.java
+++
@@ -1,8 +0,0 @@
-package com.haidi.haidimasterdata.domain.ports.out;
-
-import com.haidi.haidimasterdata.domain.pojo.CreateConsultantCommand;
-
-public interface ConsultantPersistencePort {
-    Long saveConsultant(CreateConsultantCommand createConsultantCommand);
-
-}
